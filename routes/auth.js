92a1bb4e42b1b61f4f7f47a85fc3e535635d0729
Christian Villamin <ChristianVillamin31@gmail.com>
2019-08-31 18:52:04 +0800

    auth middleware added

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -10,8 +11,14 @@ const User = require('../models/User');
 // @route     GET api/users
 // @desc      Get logged in user
 // @access    Private
-router.get('/', (request, response) => {
-  response.send('Get logged in user');
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route     POST api/users
@@ -23,25 +30,25 @@ router.post(
     check('email', 'Please include a valid email.').isEmail(),
     check('password', 'Please includ a valid password.').exists()
   ],
-  async (request, response) => {
-    const errors = validationResult(request);
+  async (req, res) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return response.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password } = request.body;
+    const { email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
 
       if (!user) {
-        return response.status(400).json({ message: 'Invalid E-mail' });
+        return res.status(400).json({ message: 'Invalid E-mail' });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
 
       if (!isMatch) {
-        return response.status(400).json({ message: 'Invalid Password' });
+        return res.status(400).json({ message: 'Invalid Password' });
       }
 
       const payload = {
@@ -58,12 +65,12 @@ router.post(
         },
         (error, token) => {
           if (error) throw error;
-          response.json({ token });
+          res.json({ token });
         }
       );
     } catch (error) {
       console.error(error.message);
-      response.status(500).send('Server Error');
+      res.status(500).send('Server Error');
     }
   }
 );
