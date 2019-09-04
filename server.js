6f7f8b755ccf8ab837e4f7fb8025f0435a5806c8
Christian Villamin <ChristianVillamin31@gmail.com>
2019-09-04 23:55:09 +0800

    test

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use('/api/users', require('./routes/users'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
 
+console.log('test');
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}.`));
