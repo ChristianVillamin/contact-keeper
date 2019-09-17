c93f62eaf9e93edf742ed1fff77c9c64c2e546fb
Christian Villamin <ChristianVillamin31@gmail.com>
2019-09-18 04:36:44 +0800

    delete contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -79,11 +79,12 @@ router.put('/:id', auth, async (req, res) => {
   try {
     let contact = await Contact.findById(req.params.id);
 
-    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+    if (!contact)
+      return res.status(404).json({ message: 'Contact not found.' });
 
     // Make sure user owns contact
     if (contact.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'Not authorized.' });
+      return res.status(401).json({ message: 'Not authorized.' });
     }
 
     contact = await Contact.findByIdAndUpdate(
@@ -116,7 +117,7 @@ router.delete('/:id', auth, async (req, res) => {
 
     await Contact.findByIdAndRemove(req.params.id);
 
-    res.json({ msg: 'Contact removed' });
+    res.json({ message: 'Contact removed' });
   } catch (error) {
     console.error(error.message);
     res.status(500).send('Server Error');
