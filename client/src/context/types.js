5e49828a6ecdf4e568170853208f0c3d125aa110
Christian Villamin <ChristianVillamin31@gmail.com>
2019-09-18 02:15:57 +0800

    add contact

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -9,6 +9,7 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const SET_CURRENT = 'SET_CURRENT';
+export const CONTACT_ERROR = 'CONTACT_ERROR';
 
 // AUTH
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
--- routes/contacts.js
+++ routes/contacts.js
@@ -106,11 +106,12 @@ router.delete('/:id', auth, async (req, res) => {
   try {
     let contact = await Contact.findById(req.params.id);
 
-    if (!contact) return res.status(404).json({ msg: 'Contact not found.' });
+    if (!contact)
+      return res.status(404).json({ message: 'Contact not found.' });
 
     // Make sure user owns contact
     if (contact.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: 'Not authorized.' });
+      return res.status(401).json({ message: 'Not authorized.' });
     }
 
     await Contact.findByIdAndRemove(req.params.id);
