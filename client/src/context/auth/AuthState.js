5680ea398fd671b1e900f938ae64356450495fbd
Christian Villamin <ChristianVillamin31@gmail.com>
2019-09-18 01:37:01 +0800

    user login

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -69,7 +69,29 @@ const AuthState = props => {
   };
 
   // Login User
-  const login = async () => {};
+  const login = async formData => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (error) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: error.response.data.message
+      });
+    }
+  };
 
   // Logout
   const logout = () => console.log('logout');
